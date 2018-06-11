e4e3f522fe3f34f24806f6e58d3c86889baaefea
1661sons <julia.kerouac@gmail.com>
2018-06-10 23:01:28 -0400

    changed images, created defender and attack info sections

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,77 +4,100 @@ $(document).ready(function() {
     var offender;
     var defender;
 
-    $('.obiWan').click( function() {
-        // set the offender to Obi-Wan
-        offender = characters[0];
-        console.log("your character is " + offender);
-        
-        // when obiWan is selected, move him to Your Character div
-        $('.obiWan').appendTo('#chosenOffender');
-        $('.obiWan').removeClass('characterImage');
+    function chooseOffender() {
 
-        // move all other characters to enemies section and give them a red border
-        $('.luke, .sidious, .maul').appendTo('.enemyCharacters');
-        $('.luke, .sidious, .maul').removeClass('characterImage');
-        $('.luke, .sidious, .maul').addClass('enemyImage');
-    });
-
-    $('.luke').click( function() {
-        // set the offender to Luke
-        offender = characters[1];
-        console.log("your character is " + offender);
-        
-        // when luke is selected, move him to Your Character div
-        $('.luke').appendTo('#chosenOffender');
-        $('.luke').removeClass('characterImage');
-
-        // move all other characters to enemies section and give them a red border
-        $('.obiWan, .sidious, .maul').appendTo('.enemyCharacters');
-        $('.obiWan, .sidious, .maul').removeClass('characterImage');
-        $('.obiWan, .sidious, .maul').addClass('enemyImage');
-    });
-
-    $('.sidious').click( function() {
-        // set offender to Sidious
-        offender = characters[2];
-        console.log("your character is " + offender);
-        
-        // when Luke is selected, move him to Your Character div
-        $('.sidious').appendTo('#chosenOffender');
-        $('.sidious').removeClass('characterImage');
-
-        // move all other characters to enemies section and give them a red border
-        $('.obiWan, .luke, .maul').appendTo('.enemyCharacters');
-        $('.obiWan, .luke, .maul').removeClass('characterImage');
-        $('.obiWan, .luke, .maul').addClass('enemyImage');
-    });
-
-    $('.maul').click( function(){
-        // set yourCharacter to Sidious
-        offender = characters[2];
-        console.log("your character is " + offender);
-        
-        // when Maul is selected, move him to Your Character div
-        $('.maul').appendTo('#chosenOffender');
-        $('.maul').removeClass('characterImage');
-
-        // move all other characters to enemies section and give them a red border
-        $('.obiWan, .luke, .sidious').appendTo('.enemyCharacters');
-        $('.obiWan, .luke, .sidious').removeClass('characterImage');
-        $('.obiWan, .luke, .sidious').addClass('enemyImage');
-    });
-
-    // if the offender is not Obi-Wan, that means Obi-Wan is an ememy
-    if (offender != characters[0]) {
-        // when Obi-Wan is clicked, make him the Defender
         $('.obiWan').click( function() {
+            // set the offender to Obi-Wan
+            offender = characters[0];
+            console.log("your character is " + offender);
+            
+            // when obiWan is selected, move him to Your Character div
+            $('.obiWan').appendTo('#chosenOffender');
+            $('.obiWan').removeClass('characterImage');
+
+            // move all other characters to enemies section and give them a red border
+            $('.luke, .sidious, .maul').appendTo('.enemyCharacters');
+            $('.luke, .sidious, .maul').removeClass('characterImage');
+            $('.luke, .sidious, .maul').addClass('enemyImage');
+        });
+
+        $('.luke').click( function() {
+            // set the offender to Luke
+            offender = characters[1];
+            console.log("your character is " + offender);
+            
+            // when luke is selected, move him to Your Character div
+            $('.luke').appendTo('#chosenOffender');
+            $('.luke').removeClass('characterImage');
+
+            // move all other characters to enemies section and give them a red border
+            $('.obiWan, .sidious, .maul').appendTo('.enemyCharacters');
+            $('.obiWan, .sidious, .maul').removeClass('characterImage');
+            $('.obiWan, .sidious, .maul').addClass('enemyImage');
+        });
+
+        $('.sidious').click( function() {
+            // set offender to Sidious
+            offender = characters[2];
+            console.log("your character is " + offender);
+            
+            // when Luke is selected, move him to Your Character div
+            $('.sidious').appendTo('#chosenOffender');
+            $('.sidious').removeClass('characterImage');
+
+            // move all other characters to enemies section and give them a red border
+            $('.obiWan, .luke, .maul').appendTo('.enemyCharacters');
+            $('.obiWan, .luke, .maul').removeClass('characterImage');
+            $('.obiWan, .luke, .maul').addClass('enemyImage');
+        });
+
+        $('.maul').click( function(){
+            // set yourCharacter to Sidious
+            offender = characters[2];
+            console.log("your character is " + offender);
+            
+            // when Maul is selected, move him to Your Character div
+            $('.maul').appendTo('#chosenOffender');
+            $('.maul').removeClass('characterImage');
+
+            // move all other characters to enemies section and give them a red border
+            $('.obiWan, .luke, .sidious').appendTo('.enemyCharacters');
+            $('.obiWan, .luke, .sidious').removeClass('characterImage');
+            $('.obiWan, .luke, .sidious').addClass('enemyImage');
+        });
+    }
+    chooseOffender();
+
+    function chooseDefender() {
+
+        // when Obi-Wan is clicked, make him the Defender
+        $('.obiWan .enemyImage').click( function() {
             defender = characters[0];
             console.log(defender);
 
+            $('.obiWan').appendTo('#chosenDefender');
+        });
+
+        // when Luke is clicked, make him the defender
+        $('.luke .enemyImage').click( function() {
+            defender = characters[1];
+            console.log(defender);
+
+            $('.luke').appendTo('#chosenDefender');
         
         });
-        
+
+        // when Luke is clicked, make him the defender
+        $('.sidious .enemyImage').click( function(){
+            defender = characters[2];
+            console.log(defender);
+
+            $('.sidious').appendTo('#chosenDefender');
+
+        });
     }
+    chooseDefender();
+        
 
 
 
