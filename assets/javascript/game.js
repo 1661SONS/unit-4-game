2a861bbec20914b7a36ccbcb98d8649764269f15
1661sons <julia.kerouac@gmail.com>
2018-06-12 18:39:44 -0400

    saving homework checkpoint

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,29 @@
-$(document).ready(function() {
+$(document).ready( function() {
 
     var characters = ['obi-wan', 'luke', 'sidious', 'maul'];
+    var offender;
+    var defender;
+    var offenderDamage = $('#offenderDamage').text(0);
+    var defenderDamage = $('#defenderDamage').text(0);
+
+    // $('.noDefender, .offenderAttack, .defenderAttack').addClass('hide');
+    
+    if (defender = []) {
+        $('.btn').click( function(){
+            $('.noDefender').removeClass('hide');
+
+        });
+    }
+    
     
     function chooseDefender() {
-
-        var defender;
     
         // when Obi-Wan is clicked, make him the Defender
         $('.obiWan').click( function() {
             defender = characters[0];
             console.log("the defender is " + defender);
     
-            $('.obiWan').appendTo('#chosenDefender');
+            $('.enemyobiWan').appendTo('#chosenDefender');
         });
     
         // when Luke is clicked, make him the defender
@@ -19,7 +31,7 @@ $(document).ready(function() {
             defender = characters[1];
             console.log("the defender is " + defender);
     
-            $('.luke').appendTo('#chosenDefender');
+            $('.enemyLuke').appendTo('#chosenDefender');
         
         });
     
@@ -28,7 +40,7 @@ $(document).ready(function() {
             defender = characters[2];
             console.log("the defender is " + defender);
     
-            $('.sidious').appendTo('#chosenDefender');
+            $('.enemySidious').appendTo('#chosenDefender');
     
         });
     
@@ -37,16 +49,14 @@ $(document).ready(function() {
             defender = characters[3];
             console.log("the defender is " + defender);
     
-            $('.maul').appendTo('#chosenDefender');
+            $('.enemyMaul').appendTo('#chosenDefender');
     
         });
     }
-    chooseDefender();
-
-    var offender;
-
-    function chooseOffender() {
-
+   chooseDefender();
+    
+   function chooseOffender() {
+    
         $('.obiWan').click( function() {
             // set the offender to Obi-Wan
             offender = characters[0];
@@ -55,8 +65,12 @@ $(document).ready(function() {
             // when obiWan is selected, move him to Your Character div
             $('.obiWan').appendTo('#chosenOffender');
 
-            // move all other characters to enemies section and give them a red border
-            $('.luke, .sidious, .maul').appendTo('.enemyCharacters').addClass('enemyImage');
+            // move all other characters to enemies section and replace their white border with a red one
+            $('.luke').appendTo('.enemyLuke').addClass('enemyImage');
+            $('.sidious').appendTo('.enemySidious').addClass('enemyImage');
+            $('.maul').appendTo('.enemyMaul').addClass('enemyImage');
+            
+            $('.enemyObiWan').detach();
         });
 
         $('.luke').click( function() {
@@ -67,8 +81,12 @@ $(document).ready(function() {
             // when luke is selected, move him to Your Character div
             $('.luke').appendTo('#chosenOffender');
 
-            // move all other characters to enemies section and give them a red border
-            $('.obiWan, .sidious, .maul').appendTo('.enemyCharacters').addClass('enemyImage');
+            // move all other characters to enemies section and replace their white border with a red one
+            $('.obiWan').appendTo('.enemyObiWan').addClass('enemyImage');
+            $('.sidious').appendTo('.enemySidious').addClass('enemyImage');
+            $('.maul').appendTo('.enemyMaul').addClass('enemyImage');
+
+            $('.enemyLuke').detach();
         });
 
         $('.sidious').click( function() {
@@ -79,8 +97,13 @@ $(document).ready(function() {
             // when Luke is selected, move him to Your Character div
             $('.sidious').appendTo('#chosenOffender');
 
-            // move all other characters to enemies section and give them a red border
-            $('.obiWan, .luke, .maul').appendTo('.enemyCharacters').addClass('enemyImage');
+            // move all other characters to enemies section and replace their white border with a red one
+
+            $('.obiWan').appendTo('.enemyObiWan').addClass('enemyImage');
+            $('.luke').appendTo('.enemyLuke').addClass('enemyImage');
+            $('.maul').appendTo('.enemyMaul').addClass('enemyImage');
+
+            $('.enemySidious').detach();
         });
 
         $('.maul').click( function(){
@@ -91,23 +114,31 @@ $(document).ready(function() {
             // when Maul is selected, move him to Your Character div
             $('.maul').appendTo('#chosenOffender');
 
-            // move all other characters to enemies section and give them a red border
-            $('.obiWan, .luke, .sidious').appendTo('.enemyCharacters').addClass('enemyImage');
+            // move all other characters to enemies section and replace their white border with a red one
+            $('.obiWan').appendTo('.enemyObiWan').addClass('enemyImage');
+            $('.luke').appendTo('.enemyLuke').addClass('enemyImage');
+            $('.sidious').appendTo('.enemySidious').addClass('enemyImage');
+
+            $('.enemyMaul').detach();
         });
     }
     chooseOffender();
 
+    // left to do:
+    // 1. stop chooseOffender function from running when an offender has already been chosen so that chooseDefender runs and the chosen defender from the enemies will be appended to the defender div instead of added onto the offender div (i could not get this to work - even with one() and toggle()).
+
+    // 2. create attacking functions to dynamically change health points and attackSection data.
 
 
 
 
 
-    // luke attacks in factors of 5? when is selected character
-    // obi-wan attacks in factors of 8 when is selecter character
+
+
+    // luke attacks in factors of 5? when is chosen offender
+    // obi-wan attacks in factors of 8 when is chosen offender
     // maul attacks for 25
     // sidious attacks for 20
-
-    // add button that either forces page refresh or restarts shit
 
 
 
